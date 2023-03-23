d27d66cbfabb60542b96884f9c84e2537c710628
it22199508 <127378334+it22199508@users.noreply.github.com>
2023-03-23 05:20:21 +0000

    ok

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ int main() {
  return 0;
 }*/
 
-#include <iostream>
+/*#include <iostream>
 using namespace std;
 int volume(int height, int width, int length);
 int main() {
@@ -52,4 +52,49 @@ int volume(int height, int width, int length){
 	vol= height*width*length;
 	
 	return vol;
+}
+*/
+#include <iostream>
+using namespace std;
+int volume(int height, int width, int length);
+
+struct structname {
+  int length;
+  int width;
+  int height;
+ }box1,box2;
+
+
+int main() {
+
+ int totalVolume;
+ 
+ // 4. Input the height, width, lenght of box1 and box2
+ cout << "Enter Box 1 Height : ";
+ cin >> box1.height;
+ cout << "Enter Box 1 Width : ";
+ cin >> box1.width;
+ cout << "Enter Box 1 Length : ";
+cin >> box1.length;
+ 
+ cout << "Enter Box 2 Height : ";
+ cin >> box2.height;
+ cout << "Enter Box 2 Width : ";
+ cin >> box2.width;
+ cout << "Enter Box 2 Length : ";
+ cin >> box2.length;
+ 
+ // 5. Replace the coding below to pass the Box type structure
+ totalVolume = volume(box1.height,box1.width ,box1.length )
+ + volume(box2.height,box2.width ,box2.length);
+ 
+ 
+ cout << "Volume of Box is " << totalVolume << endl;
+ 
+ return 0;
+}
+int volume(int height, int width, int length){
+  int vol;
+  vol=height*length*width;
+  return vol;
 }
